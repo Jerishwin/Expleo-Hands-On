bf527108ea1d3563b628c0339ed762fe9d86056e
Jerishwin Joseph <jerishwinj@gmail.com>
2026-03-12 13:04:27 +0530

    11-03-2026

---
+++ ControlFlow/src/com/practice/AssertionDemo.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+
+public class AssertionDemo {
+	public static void main(String[]args) {
+		int n =55;
+		assert n>=60:"rong value";
+		System.out.print("Value: "+n);
+	}
+}
