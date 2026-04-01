d0e4f0410a9efe3b5b8ac4c18ecef737bf8d4b57
Jerishwin Joseph <jerishwinj@gmail.com>
2026-04-01 11:45:48 +0530

    1-4-2026

--- SeleniumBasics/src/main/java/com/demo/Demo.java
+++ SeleniumBasics/src/main/java/com/demo/Demo.java
@@ -3,15 +3,13 @@ package com.demo;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+
 
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		FirefoxOptions options = new FirefoxOptions();
-		options.addArguments("--headless");
-		WebDriver driver =  new FirefoxDriver(options);
+		WebDriver driver =  new FirefoxDriver();
 		driver.get("https://www.flipkart.com/");
 		String title = driver.getTitle();
 		int length = title.length();
