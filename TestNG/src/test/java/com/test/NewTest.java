c54eadeedf368183502cb177a8cafc0d8a78c989
Jerishwin Joseph <jerishwinj@gmail.com>
2026-04-17 11:31:28 +0530

    17-04-2026

--- TestNG/src/test/java/com/test/NewTest.java
+++ TestNG/src/test/java/com/test/NewTest.java
@@ -6,37 +6,62 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class NewTest {
 	public WebDriver driver;
-	
-	@AfterTest
-	  public void afterTest() {
-		  driver.close();
-	  }
-	
-  @Test
-  public void f() {
-	  driver.findElement(By.xpath("//a[text()='Log in']")).click();
-	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	  driver.findElement(By.id("loginusername")).sendKeys("Banton");
-	  driver.findElement(By.id("loginpassword")).sendKeys("JeriJose");
-	  driver.findElement(By.xpath("//button[text()='Log in']")).click();
 
-	  
-  }
-  
-  @BeforeTest
-  public void beforeTest() {
-	  ChromeOptions options = new ChromeOptions();
-	  options.addArguments("--start-maximized");
-	  options.addArguments("--headless");
-	  driver = new ChromeDriver(options);
-	  driver.get("https://www.demoblaze.com/");
-	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-  }
-  
+	@AfterMethod
+	public void afterTest() {
+		driver.quit();
+	}
+
+	@Test
+	public void Login() {
+		driver.findElement(By.xpath("//a[text()='Log i']")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.id("loginusername")).sendKeys("Banton");
+		driver.findElement(By.id("loginpassword")).sendKeys("JeriJose");
+		driver.findElement(By.xpath("//button[text()='Log in']")).click();
+
+	}
+
+	@Test(dependsOnMethods = "Login")
+	public void WrongLoginPassword() {
+		driver.findElement(By.xpath("//a[text()='Log in']")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.id("loginusername")).sendKeys("Banton");
+		driver.findElement(By.id("loginpassword")).sendKeys("Jeri");
+		driver.findElement(By.xpath("//button[text()='Log in']")).click();
+
+	}
+	
+	
+	@Test(dependsOnMethods = "Login")
+	public void WrongLoginUser() {
+		driver.findElement(By.xpath("//a[text()='Log in']")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.id("loginusername")).sendKeys("Jack123");
+		driver.findElement(By.id("loginpassword")).sendKeys("JeriJose");
+		driver.findElement(By.xpath("//button[text()='Log in']")).click();
+		// driver.switchTo().alert().dismiss();
+
+	}
+
+	@BeforeMethod
+	public void beforeTest() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
+		driver.get("https://www.demoblaze.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+
 }
