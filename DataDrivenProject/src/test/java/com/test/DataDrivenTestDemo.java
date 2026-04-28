80b2fd46d3f2c8cba8e46e47c8badb68335d0e7a
Jerishwin Joseph <jerishwinj@gmail.com>
2026-04-28 10:05:09 +0530

    28-4-26

--- DataDrivenProject/src/test/java/com/test/DataDrivenTestDemo.java
+++ DataDrivenProject/src/test/java/com/test/DataDrivenTestDemo.java
@@ -2,6 +2,8 @@ package com.test;
 
 import java.time.Duration;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -22,16 +24,19 @@ import org.testng.annotations.Test;
 public class DataDrivenTestDemo {
 	public WebDriver driver;
 	public WebDriverWait wait; 
+	public static Logger log = LogManager.getLogger(DataDrivenTestDemo.class);
 
 	@AfterMethod
 	public void teardon() {
 		driver.quit();
+		log.info("The Browser is closed");
 	}
 
 	@Test(dataProvider="validExcelData" , dataProviderClass = com.utilities.ExcelUtils.class)
 
 	public void Login(String name,String pass) {
 		
+		log.info("The valid login is Started");
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		driver.findElement(By.xpath("//div[@id='top-links']/ul/li[2]")).click();
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='top-links']/ul/li[2]/ul/li[2]"))).click();
@@ -40,11 +45,14 @@ public class DataDrivenTestDemo {
 		driver.findElement(By.xpath("//input[@value='Login']")).click();
 		WebElement id = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//h2[1]")));
 		Assert.assertEquals(id.getText(), "My Account");
+		log.info("Login is sucessfull");
 
 	}
 	
 	@Test(dataProvider="invalidExcelData" , dataProviderClass = com.utilities.ExcelUtils.class)
 	public void WrongLoginUser(String name,String pass) {
+		
+		log.info("The invalid login is Started");
 		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		driver.findElement(By.xpath("//div[@id='top-links']/ul/li[2]")).click();
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='top-links']/ul/li[2]/ul/li[2]"))).click();
@@ -52,39 +60,52 @@ public class DataDrivenTestDemo {
 		driver.findElement(By.id("input-password")).sendKeys(pass);
 		driver.findElement(By.xpath("//input[@value='Login']")).click();
 		WebElement id = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='alert alert-danger alert-dismissible']")));
-		Assert.assertEquals(id.getText(), "Warning: No match for E-Mail Address and/or Password.");
+		if(id.getText().equals("Warning: No match for E-Mail Address and/or Password.")) {
+			Assert.assertEquals(id.getText(), "Warning: No match for E-Mail Address and/or Password.");
+		}else {
+			Assert.assertEquals(id.getText(), "Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour.");
+		}
+		
+		log.warn("Login is unsucessfull");
 
 	}
 	
 	@Test
 	@Parameters({"validName"})
 	public void validSearch(String name) {
+		
+		log.info("The valid Search is Started");
 		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		driver.findElement(By.xpath("//input[@name='search']")).sendKeys(name);
 		driver.findElement(By.xpath("//input[@name='search']")).sendKeys(Keys.ENTER);
 		WebElement id = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2")));
 		Assert.assertEquals(id.getText(), "Products meeting the search criteria");
+		log.info("Search is Sucessfull");
 
 	}
 	
 	@Test
 	@Parameters({"invalidName"})
 	public void invalidSearch(String name) {
+		
+		log.info("The invalid Search is Started");
 		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		driver.findElement(By.xpath("//input[@name='search']")).sendKeys(name);
 		driver.findElement(By.xpath("//input[@name='search']")).sendKeys(Keys.ENTER);
-		WebElement id = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("///p[2]")));
+		WebElement id = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='content']/p[2]")));
 		Assert.assertEquals(id.getText(), "There is no product that matches the search criteria.");
-
+		log.error("Product are not found");
 	}
 	
 
 	@BeforeMethod
 	public void beforeTest() {
-		FirefoxOptions options = new FirefoxOptions();
+		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized");
-		driver = new FirefoxDriver(options);
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
 		driver.get("https://tutorialsninja.com/demo/");
+		log.info("Driver is opened");
 		
 	}
 }
